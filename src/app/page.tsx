4407917cecfd25932aca6c4c23e339fe966786ec
nekkuzuria <amandafarliana10@gmail.com>
2024-08-05 23:45:29 +0700

    add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 import IndexPage from "../pages/home";
-import Head from "next/head";
+
+export const metadata = {
+  title: "Let Me Cook",
+  description:
+    "Discover new recipes based on the ingredients you have at home. Input ingredients and get suggestions from Cookpad.",
+};
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Let Me Cook</title>
-        <meta
-          name="description"
-          content="Discover new recipes based on the ingredients you have at home. Input ingredients and get suggestions from Cookpad."
-        />
-        <meta name="author" content="nekkuzuria" />
-      </Head>
       <IndexPage />
     </>
   );
