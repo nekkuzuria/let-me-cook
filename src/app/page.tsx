77bd619523581c396267827c00d0676860baafe2
nekkuzuria <amandafarliana10@gmail.com>
2024-08-05 21:15:05 +0700

    add meta description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 import IndexPage from "../pages/home";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Let Me Cook</title>
+        <meta
+          name="description"
+          content="Discover new recipes based on the ingredients you have at home. Input ingredients and get suggestions from Cookpad."
+        />
+        <meta name="author" content="nekkuzuria" />
+      </Head>
       <IndexPage />
     </>
   );
